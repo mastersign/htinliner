19fe42c57fee6084b2e454da4f0e0173c947b22b
Tobias Kiertscher <dev@mastersign.de>
2014-08-21 15:46:02 +0200

    implemented generalized version of the inline using cheerio

--- src/index.js
+++ src/index.js
@@ -1,44 +1,153 @@
 var through = require('through2');
 var path = require('path');
 var fs = require('fs');
+var url = require('url');
+var cheerio = require('cheerio');
 
-var removeSize = function(svgSource) {
-	var widthPattern = /<svg([^>]*?)\swidth\s*=\s*(["'])([^\2]*?)\2([^>]*?)>/
-	var heightPattern = /<svg([^>]*?)\sheight\s*=\s*(["'])([^\2]*?)\2([^>]*?)>/
-	var m;
-	var replFn = function(match, pre, _, _, post) {
-		return '<svg' + pre + post + '>'
-	};
-	svgSource = svgSource.replace(widthPattern, replFn);
-	svgSource = svgSource.replace(heightPattern, replFn);
-	return svgSource;
+var defOpt = { 
+	inlineStylesheets: true,
+	inlineScripts: true,
+	inlineSvgImages: true,
+	svgRemoveSize: false,
+	svgWrap: true,
+	svgWrapElement: 'span',
+	svgWrapClass: 'htinliner'
 };
 
-var inline = function(htmlSource, sourcePath) {
-	var imgPattern = /<img(\s+|\s.*?\s)src\s*=\s*(["'])([^\2]*?)\2(.*?)>/g;
+var option = function(name, opt) {
+	return opt ? (opt[name] || defOpt[name]) : defOpt[name];
+}
+
+var fixSvgSize = function(svgSource, remove, width, height) {
+	var $ = cheerio.load(svgSource, { xmlMode: true });
+	var svg = $('svg');
+	if (width && width.trim().match(/^\d+$/)) { width = width + 'px'; }
+	if (height && height.trim().match(/^\d+$/)) { height = height + 'px'; }
+	if (remove) {
+		svg.removeAttr('width');
+		svg.removeAttr('height');
+		svg.css('width', null);
+		svg.css('height', null);
+	} else {
+		if (width) {
+			svg.css('width', width);
+		} else if (!svg.css('width')) {
+			svg.css('width', svg.attr('width') + 'px');
+		}
+		if (height) {
+			svg.css('height', height);
+		} else if (!svg.css('height')) {
+			svg.css('height', svg.attr('height') + 'px');
+		}
+		svg.removeAttr('width');
+		svg.removeAttr('height');
+	}
+	return $.xml('svg');
+};
+
+var getPathFromUrl = function(src, ext) {
+	var srcUrl;
+	if (!src) return null;
+	srcUrl = url.parse(src);
+	if (!srcUrl ||
+		srcUrl.protocol ||
+		!srcUrl.pathname ||
+		(ext && srcUrl.pathname.slice(-4).toLowerCase() !== ext)) { 
+
+		return null;
+	}
+	return srcUrl.pathname;
+};
+
+var inline = function(htmlSource, sourcePath, opt) {
+	var $;
 	var buildSrcPath = function(src) {
 		return path.resolve(path.dirname(sourcePath), src);
 	};
+	sourcePath = sourcePath || '.';
 	if (htmlSource instanceof Buffer) {
-		htmlSource = htmlSource.toString();
+		htmlSource = htmlSource.toString(option('encoding', opt));
 	}
-	return htmlSource.replace(imgPattern, function(match, pre, _, src, post) {
-		return '<div class="svginline">' +
-			removeSize(fs.readFileSync(buildSrcPath(src), 'utf8')) +
-			'</div>';
-	});
+	$ = cheerio.load(htmlSource);
+
+	if (option('inlineStylesheets', opt)) {
+		$('link[rel="stylesheet"]', 'head').each(function(i, elem) {
+			var src = getPathFromUrl($(elem).attr('href'));
+			var styleDoc, styleTag, typeAttr;
+			var styleSource;
+			if (!src) return;
+			styleDoc = cheerio.load('<style></style>');
+			styleTag = styleDoc('style');
+			typeAttr = $(elem).attr('type');
+			if (typeAttr) {
+				styleTag.attr('type', typeAttr);
+			}
+			styleSource = fs.readFileSync(buildSrcPath(src), 'utf8');
+			styleTag.append('\n' + styleSource);
+			$(elem).replaceWith(styleDoc.html());
+		});
+	}
+	if (option('inlineScripts', opt)) {
+		$('script').each(function(i, elem) {
+			var src = getPathFromUrl($(elem).attr('src'));
+			var scriptDoc, scriptTag;
+			var scriptSource;
+			if (!src) return;
+			scriptSource = fs.readFileSync(buildSrcPath(src), 'utf8');
+			scriptDoc = cheerio.load('<script></script>');
+			scriptTag = scriptDoc('script');
+			scriptTag.append('\n' + scriptSource);
+			scriptTag.attr('type', $(elem).attr('type'));
+			scriptTag.attr('language', $(elem).attr('language'));
+			if ($(elem).contents().length > 0) {
+				$(elem).removeAttr('src');
+				$(elem).before(scriptDoc.html());
+			} else {
+				$(elem).replaceWith(scriptDoc.html());
+			}
+		});
+	}
+	if (option('inlineSvgImages', opt)) {
+		$('img', 'body').each(function(i, elem) {
+			var src = getPathFromUrl($(elem).attr('src'), '.svg');
+			var width = $(elem).css('width') || $(elem).attr('width');
+			var height = $(elem).css('height') || $(elem).attr('height');
+			var svgData, svgSource;
+			var prefix, postfix;
+			if (!src) return;
+			svgData = fs.readFileSync(buildSrcPath(src));
+			svgSource = fixSvgSize(svgData, option('svgRemoveSize', opt), width, height);
+			if (option('svgWrap', opt)) {
+				prefix = '<' + option('svgWrapElement', opt) + ' class="' +
+					option('svgWrapClass', opt) + '">';
+				postfix = '</' + option('svgWrapElement', opt) + '>';
+			} else {
+				prefix = '';
+				postfix = '';
+			}
+			$(elem).replaceWith(prefix + svgSource + postfix);
+		});
+	}
+	return $.xml();
 };
 
-var svginline = function() {
+var htinliner = function() {
+	if (arguments.length > 0 && 
+		(typeof(arguments[0]) === 'string' ||
+		 arguments[0] instanceof Buffer)) {
+		return inline(arguments[0], arguments[1], arguments[2]);
+	}
+	var opt = arguments[0];
 	return through.obj(function(file, enc, cb) {
 		var sourcePath = path.resolve(file.cwd, file.path);
+		var sourceHtml;
 		if (file.isNull()) {
 			// pass
 			this.push(file);
 			cb();
 		} else if (file.isBuffer()) {
-			// extract asynchronously
-			file.contents = new Buffer(inline(file.contents.toString(enc), sourcePath));
+			sourceHtml = file.contents;
+			file.contents = new Buffer(inline(sourceHtml, sourcePath, opt));
 			this.push(file);
 			cb();
 		} else if (file.isStream()) {
@@ -47,5 +156,4 @@ var svginline = function() {
 	});
 };
 
-svginline.inlineSvg = inline;
-module.exports = svginline;
+module.exports = htinliner;
